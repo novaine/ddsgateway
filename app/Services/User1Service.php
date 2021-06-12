82e0cf9630dfdec00479a5f6d4c86b32ae220558
novaine <novaabalorio@gmail.com>
2021-06-13 04:31:52 +0800

    Changes in June 11, 2021

---
+++ app/Services/User1Service.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalService;
+
+class User1Service {
+
+    use ConsumesExternalService;
+
+    /** 
+     * The base uri to consume the User1 Service
+     * @var string
+     */
+
+     public $baseUri;
+
+     /** 
+     * The srecret to consume the User1 Service
+     * @var string
+     */
+    public $secret;
+
+     public function __construct()
+     {
+         $this->baseUri = config('services.users1.base_uri');
+         $this->secret = config('services.users1.secret');
+     }
+
+     public function obtainUsers1()
+     {
+         return $this->performRequest('GET','/users');
+     }
+
+     public function createUser1($data)
+     {
+         return $this->performRequest('POST','/users', $data);
+     }
+
+     public function obtainUser1($id)
+     {
+         return $this->performRequest('GET',"/users/{$id}");
+     }
+
+     public function editUser1($data,$id)
+     {
+         return $this->performRequest('PUT',"/users/{$id}", $data);
+     }
+
+     public function deleteUser1($id)
+     {
+         return $this->performRequest('DELETE', "/users/{$id}");
+     }
+}
---
+++ app/Services/User2Service.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Services;
+
+use App\Traits\ConsumesExternalService;
+
+class User2Service {
+
+    use ConsumesExternalService;
+
+    /** 
+     * The base uri to consume the User1 Service
+     * @var string
+     */
+
+     public $baseUri;
+
+    /** 
+     * The srecret to consume the User2 Service
+     * @var string
+     */
+    public $secret;
+
+     public function __construct()
+     {
+         $this->baseUri = config('services.users2.base_uri');
+         $this->secret = config('services.users2.secret');
+     }
+
+     public function obtainUsers2()
+     {
+         return $this->performRequest('GET','/users');
+     }
+
+     public function createUser2($data)
+     {
+         return $this->performRequest('POST','/users', $data);
+     }
+
+     public function obtainUser2($id)
+     {
+         return $this->performRequest('GET',"/users/{$id}");
+     }
+
+     public function editUser2($data,$id)
+     {
+         return $this->performRequest('PUT',"/users/{$id}", $data);
+     }
+
+     public function deleteUser2($id)
+     {
+         return $this->performRequest('DELETE', "/users/{$id}");
+     }
+}
